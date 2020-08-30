3fdb646d169dfcb765c88d22e7fbfed5fac0d0e5
Isaac Alejandro Pimentel Morales <isaacapimentelm@gmail.com>
2020-08-30 11:53:14 -0500

    patron descubierto

--- Mosaico.c
+++ Mosaico.c
@@ -29,6 +29,7 @@ void marcarContorno(float x1, float y1, float x2, float y2, float x3, float y3, 
 
 
 void display(void) {
+	//Cuadrante 1 rectangulos
 	glColor3f(0.96, 0.84, 0.13);//amarillo
 	drawQUAD(0.4,0.0,0.55,0.0,0.0,0.55,0.0,0.4);
 	drawQUAD(1.0,0.45,1.0,0.6,0.6,1.0,0.45,1.0);
@@ -47,6 +48,63 @@ void display(void) {
 	marcarContorno(1.0,0.3,1.0,0.45,0.45,1.0,0.3,1.0);
 	marcarContorno(0.7,0.0,0.77,0.07,0.07,0.77,0.0,0.7);
 	marcarContorno(0.93,0.23,1.0,0.3,0.3,1.0,0.23,0.93);
+	//Cuadrante 2 rectangulos------------------------------
+	glColor3f(0.96, 0.84, 0.13);//amarillo
+	drawQUAD(-0.4,0.0,-0.55,0.0,0.0,0.55,0.0,0.4);
+	drawQUAD(-1.0,0.45,-1.0,0.6,-0.6,1.0,-0.45,1.0);
+	glColor3f(0.92, 0.65, 0.082);//naranja
+	drawQUAD(-0.55,0.0,-0.7,0.0,0.0,0.7,0.0,0.55);
+	drawQUAD(-1.0,0.3,-1.0,0.45,-0.45,1.0,-0.3,1.0);
+	glColor3f(0.02, 0.02, 0.63);//azul
+	drawQUAD(-0.7,0.0,-0.77,0.07,-0.07,0.77,0.0,0.7);
+	drawQUAD(-0.93,0.23,-1.0,0.3,-0.3,1.0,-0.23,0.93);
+	glColor3f(0.0,0.0,0.0);//negro
+	drawQUAD(-0.77,0.07,-0.93,0.23,-0.23,0.93,-0.07,0.77);
+	glLineWidth(1);
+	marcarContorno(-0.4,0.0,-0.55,0.0,0.0,0.55,0.0,0.4);
+	marcarContorno(-1.0,0.45,-1.0,0.6,-0.6,1.0,-0.45,1.0);
+	marcarContorno(-0.55,0.0,-0.7,0.0,0.0,0.7,0.0,0.55);
+	marcarContorno(-1.0,0.3,-1.0,0.45,-0.45,1.0,-0.3,1.0);
+	marcarContorno(-0.7,0.0,-0.77,0.07,-0.07,0.77,0.0,0.7);
+	marcarContorno(-0.93,0.23,-1.0,0.3,-0.3,1.0,-0.23,0.93);
+	//Cuadrante 3 rectangulos------------------------------
+	glColor3f(0.96, 0.84, 0.13);//amarillo
+	drawQUAD(-0.4,0.0,-0.55,0.0,0.0,-0.55,0.0,-0.4);
+	drawQUAD(-1.0,-0.45,-1.0,-0.6,-0.6,-1.0,-0.45,-1.0);
+	glColor3f(0.92, 0.65, 0.082);//naranja
+	drawQUAD(-0.55,0.0,-0.7,0.0,0.0,-0.7,0.0,-0.55);
+	drawQUAD(-1.0,-0.3,-1.0,-0.45,-0.45,-1.0,-0.3,-1.0);
+	glColor3f(0.02, 0.02, 0.63);//azul
+	drawQUAD(-0.7,-0.0,-0.77,-0.07,-0.07,-0.77,-0.0,-0.7);
+	drawQUAD(-0.93,-0.23,-1.0,-0.3,-0.3,-1.0,-0.23,-0.93);
+	glColor3f(0.0,0.0,0.0);//negro
+	drawQUAD(-0.77,-0.07,-0.93,-0.23,-0.23,-0.93,-0.07,-0.77);
+	glLineWidth(1);
+	marcarContorno(-0.4,0.0,-0.55,0.0,0.0,-0.55,0.0,-0.4);
+	marcarContorno(-1.0,-0.45,-1.0,-0.6,-0.6,-1.0,-0.45,-1.0);
+	marcarContorno(-0.55,0.0,-0.7,0.0,0.0,-0.7,0.0,-0.55);
+	marcarContorno(-1.0,-0.3,-1.0,-0.45,-0.45,-1.0,-0.3,-1.0);
+	marcarContorno(-0.7,-0.0,-0.77,-0.07,-0.07,-0.77,-0.0,-0.7);
+	marcarContorno(-0.93,-0.23,-1.0,-0.3,-0.3,-1.0,-0.23,-0.93);
+	//Cuadrante 4 rectangulos------------------------------
+	glColor3f(0.96, 0.84, 0.13);//amarillo
+	drawQUAD(0.4,0.0,0.55,0.0,0.0,-0.55,0.0,-0.4);
+	drawQUAD(1.0,-0.45,1.0,-0.6,0.6,-1.0,0.45,-1.0);
+	glColor3f(0.92, 0.65, 0.082);//naranja
+	drawQUAD(0.55,0.0,0.7,0.0,0.0,-0.7,0.0,-0.55);
+	drawQUAD(1.0,-0.3,1.0,-0.45,0.45,-1.0,0.3,-1.0);
+	glColor3f(0.02, 0.02, 0.63);//azul
+	drawQUAD(0.7,0.0,0.77,-0.07,0.07,-0.77,0.0,-0.7);
+	drawQUAD(0.93,-0.23,1.0,-0.3,0.3,-1.0,0.23,-0.93);
+	glColor3f(0.0,0.0,0.0);//negro
+	drawQUAD(0.77,-0.07,0.93,-0.23,0.23,-0.93,0.07,-0.77);
+ 	glLineWidth(1);
+	marcarContorno(0.4,0.0,0.55,0.0,0.0,-0.55,0.0,-0.4);
+	marcarContorno(1.0,-0.45,1.0,-0.6,0.6,-1.0,0.45,-1.0);
+	marcarContorno(0.55,0.0,0.7,0.0,0.0,-0.7,0.0,-0.55);
+	marcarContorno(1.0,-0.3,1.0,-0.45,0.45,-1.0,0.3,-1.0);
+	marcarContorno(0.7,0.0,0.77,-0.07,0.07,-0.77,0.0,-0.7);
+	marcarContorno(0.93,-0.23,1.0,-0.3,0.3,-1.0,0.23,-0.93);
 }
 
 void teclado(unsigned char key, int xmouse, int ymouse) {
@@ -58,7 +116,7 @@ void main(int argc, char** argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB);
 	glutInitWindowPosition(50, 100);
-	glutInitWindowSize(400, 300);
+	glutInitWindowSize(500, 500);
 	glutCreateWindow("Mosaico freeglut");
 	init();
 	glutDisplayFunc(display);
