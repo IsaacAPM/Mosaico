906bea3250aa8b67dac0f184cadc390072519a60
Isaac Alejandro Pimentel Morales <isaacapimentelm@gmail.com>
2020-08-30 10:54:05 -0500

    prueba en pausa

--- Mosaico.c
+++ Mosaico.c
@@ -27,26 +27,65 @@ void marcarContorno(float x1, float y1, float x2, float y2, float x3, float y3, 
 	glFlush();
 }
 
-
-void display(void) {
-	glColor3f(0.96, 0.84, 0.13);//amarillo
-	drawQUAD(0.4,0.0,0.55,0.0,0.0,0.55,0.0,0.4);
-	drawQUAD(1.0,0.45,1.0,0.6,0.6,1.0,0.45,1.0);
-	glColor3f(0.92, 0.65, 0.082);//naranja
-	drawQUAD(0.55,0.0,0.7,0.0,0.0,0.7,0.0,0.55);
-	drawQUAD(1.0,0.3,1.0,0.45,0.45,1.0,0.3,1.0);
-	glColor3f(0.02, 0.02, 0.63);//azul
-	drawQUAD(0.7,0.0,0.77,0.07,0.07,0.77,0.0,0.7);
-	drawQUAD(0.93,0.23,1.0,0.3,0.3,1.0,0.23,0.93);
-	glColor3f(0.0,0.0,0.0);//negro
-	drawQUAD(0.77,0.07,0.93,0.23,0.23,0.93,0.07,0.77);
+void pintarCuadrante(int s1,int s2,int s3,int s4,int s5,int s6,int s7,int s8){
+	float a,b,c,d,e,f,g,h;
 	glLineWidth(1);
-	marcarContorno(0.4,0.0,0.55,0.0,0.0,0.55,0.0,0.4);
-	marcarContorno(1.0,0.45,1.0,0.6,0.6,1.0,0.45,1.0);
-	marcarContorno(0.55,0.0,0.7,0.0,0.0,0.7,0.0,0.55);
-	marcarContorno(1.0,0.3,1.0,0.45,0.45,1.0,0.3,1.0);
-	marcarContorno(0.7,0.0,0.77,0.07,0.07,0.77,0.0,0.7);
-	marcarContorno(0.93,0.23,1.0,0.3,0.3,1.0,0.23,0.93);
+	glColor3f(0.96, 0.84, 0.13);//amarillo
+	a = 0.4*s1;
+	c = 0.55*s3;
+	f = 0.55*s6;
+	h = 0.4*s8;
+	drawQUAD(a,0.0,c,0.0,0.0,f,0.0,h);
+	glColor3f(0.0,0.0,0.0);//negro
+	marcarContorno(a,0.0,b,0.0,0.0,c,0.0,d);
+	glColor3f(0.96, 0.84, 0.13);//amarillo
+	a = 1.0*s1;
+	c = 1.0*s3;
+	e = 0.6*s5;
+	drawQUAD(a,0.45,c,0.6,e,1.0,0.45,1.0);
+	glColor3f(0.0,0.0,0.0);//negro
+	marcarContorno(a,0.45,1.0,0.6,0.6,1.0,0.45,1.0);
+	/* glColor3f(0.92, 0.65, 0.082);//naranja
+	a = 0.55*s1;
+	b = 0.7*s2;
+	c = 0.7*s3;
+	d = 0.55*s4;
+	drawQUAD(a,0.0,b,0.0,0.0,c,0.0,d);
+	glColor3f(0.0,0.0,0.0);//negro
+	marcarContorno(a,0.0,b,0.0,0.0,c,0.0,d);
+	glColor3f(0.92, 0.65, 0.082);//naranja
+	a = 1.0*s1;
+	b = 1.0*s2;
+	c = 1.0*s3;
+	d = 1.0*s4;
+	drawQUAD(a,0.3,b,0.45,0.45,c,0.3,d);
+	glColor3f(0.0,0.0,0.0);//negro
+	marcarContorno(a,0.3,b,0.45,0.45,c,0.3,d);
+	glColor3f(0.02, 0.02, 0.63);//azul
+	a = 0.7*s1;
+	b = 0.77*s2;
+	c = 0.77*s3;
+	d = 0.7*s3;
+	drawQUAD(a,0.0,b,0.07,0.07,c,0.0,d);
+	glColor3f(0.0,0.0,0.0);//negro
+	marcarContorno(a,0.0,b,0.07,0.07,c,0.0,d);
+	glColor3f(0.02, 0.02, 0.63);//azul
+	a = 0.93*s1;
+	b = 1.0*s2;
+	c = 1.0*s3;
+	d = 0.93*s4;
+	drawQUAD(a,0.23,b,0.3,0.3,c,0.23,d);
+	glColor3f(0.0,0.0,0.0);//negro
+	a = 0.77;
+	b = 0.93;
+	c = 0.93;
+	d = 0.77;
+	drawQUAD(a,0.07,b,0.23,0.23,c,0.07,d); */
+}
+
+
+void display(void) {	
+	pintarCuadrante(-1,-1,1,1);
 }
 
 void teclado(unsigned char key, int xmouse, int ymouse) {
