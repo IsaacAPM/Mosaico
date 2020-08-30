65f46acc0aab0b68f7eca8f386414e1444207c9f
Isaac Alejandro Pimentel Morales <isaacapimentelm@gmail.com>
2020-08-30 09:57:29 -0500

    cuadros del primer cuadrante listos

--- Mosaico.c
+++ Mosaico.c
@@ -7,71 +7,51 @@ void init() {
 	glShadeModel(GL_SMOOTH);
 }
 
-void drawQUAD(float x1, float y1, float x2, float y2){
+void drawQUAD(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4){
 	glBegin(GL_QUADS);
 	glVertex2f(x1,y1);
 	glVertex2f(x2,y2);
-	glVertex2f(y2,x2);
-	glVertex2f(y1,x1);
+	glVertex2f(x3,y3);
+	glVertex2f(x4,y4);
 	glEnd();
 	glFlush();
 }
 
-void marcarContorno(float x1, float y1, float x2, float y2){
+void marcarContorno(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4){
 	glBegin(GL_LINE_LOOP);
 	glVertex2f(x1,y1);
 	glVertex2f(x2,y2);
-	glVertex2f(y2,x2);
-	glVertex2f(y1,x1);
+	glVertex2f(x3,y3);
+	glVertex2f(x4,y4);
 	glEnd();
 	glFlush();
 }
 
-void dibujaCuadrante(int sx, int sy){
-	glColor3f(0.96, 0.84, 0.13);//amarillo
-	drawQUAD(sx*0.4,sy*0.0,sx*0.55,sy*0.0);
-	drawQUAD(sx*1.0,sy*0.45,sx*1.0,0.6);
-	glColor3f(0.92, 0.65, 0.082);//naranja
-	drawQUAD(sx*0.55,sy*0.0,sx*0.7,sy*0.0);
-	drawQUAD(sx*1.0,sy*0.3,sx*1.0,sy*0.45);
-	glColor3f(0.02, 0.02, 0.63);//azul
-	drawQUAD(sx*0.7,sy*0.0,sx*0.77,sy*0.07);
-	drawQUAD(sx*0.93,sy*0.23,sx*1.0,sy*0.3);
-	glColor3f(0.0,0.0,0.0);//negro
-	drawQUAD(sx*0.77,sy*0.07,sx*0.93,sy*0.23);
-	glLineWidth(1);
-	marcarContorno(sx*0.4,sy*0.0,sx*0.55,sy*0.0);
-	marcarContorno(sx*0.4,sy*0.0,sx*0.55,sy*0.0);
-	marcarContorno(sx*0.55,sy*0.0,sx*0.7,sy*0.0);
-	marcarContorno(sx*1.0,sy*0.3,sx*1.0,sy*0.45);
-	marcarContorno(sx*0.7,sy*0.0,sx*0.77,sy*0.07);
-	marcarContorno(sx*0.93,sy*0.23,sx*1.0,sy*0.3);
-}
 
 void display(void) {
 	glColor3f(0.96, 0.84, 0.13);//amarillo
-	drawQUAD(0.4,0.0,0.55,0.0);
-	drawQUAD(1.0,0.45,1.0,0.6);
+	drawQUAD(0.4,0.0,0.55,0.0,0.0,0.55,0.0,0.4);
+	drawQUAD(1.0,0.45,1.0,0.6,0.6,1.0,0.45,1.0);
 	glColor3f(0.92, 0.65, 0.082);//naranja
-	drawQUAD(0.55,0.0,0.7,0.0);
-	drawQUAD(1.0,0.3,1.0,0.45);
+	drawQUAD(0.55,0.0,0.7,0.0,0.0,0.7,0.0,0.55);
+	drawQUAD(1.0,0.3,1.0,0.45,0.45,1.0,0.3,1.0);
 	glColor3f(0.02, 0.02, 0.63);//azul
-	drawQUAD(0.7,0.0,0.77,0.07);
-	drawQUAD(0.93,0.23,1.0,0.3);
+	drawQUAD(0.7,0.0,0.77,0.07,0.07,0.77,0.0,0.7);
+	drawQUAD(0.93,0.23,1.0,0.3,0.3,1.0,0.23,0.93);
 	glColor3f(0.0,0.0,0.0);//negro
-	drawQUAD(0.77,0.07,0.93,0.23);
+	drawQUAD(0.77,0.07,0.93,0.23,0.23,0.93,0.07,0.77);
 	glLineWidth(1);
-	marcarContorno(0.4,0.0,0.55,0.0);
-	marcarContorno(0.55,0.0,0.7,0.0);
-	marcarContorno(0.7,0.0,0.77,0.07);
-	marcarContorno(0.93,0.23,1.0,0.3);
-	marcarContorno(1.0,0.3,1.0,0.45);
-	marcarContorno(1.0,0.45,1.0,0.6);
-	//dibujaCuadrante(-1,1);
+	marcarContorno(0.4,0.0,0.55,0.0,0.0,0.55,0.0,0.4);
+	marcarContorno(1.0,0.45,1.0,0.6,0.6,1.0,0.45,1.0);
+	marcarContorno(0.55,0.0,0.7,0.0,0.0,0.7,0.0,0.55);
+	marcarContorno(1.0,0.3,1.0,0.45,0.45,1.0,0.3,1.0);
+	marcarContorno(0.7,0.0,0.77,0.07,0.07,0.77,0.0,0.7);
+	marcarContorno(0.93,0.23,1.0,0.3,0.3,1.0,0.23,0.93);
 }
 
 void teclado(unsigned char key, int xmouse, int ymouse) {
-	if (key == 'r') printf("tecla r\n");
+	float a = 0.25;
+	if (key == 'r') printf("tecla r %f\n",a);
 }
 
 void main(int argc, char** argv) {
