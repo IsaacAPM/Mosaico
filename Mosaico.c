419bdb122bab773e23d48b6f54dbe1337f73b00a
Isaac Alejandro Pimentel Morales <isaacapimentelm@gmail.com>
2020-08-30 12:39:30 -0500

    funcion cuadrantes exitosa

--- Mosaico.c
+++ Mosaico.c
@@ -29,7 +29,7 @@ void marcarContorno(float x1, float y1, float x2, float y2, float x3, float y3, 
 
 void pintarCuadrante(int s1,int s2,int s3,int s4,int s5,int s6,int s7,int s8){
 	float a,b,c,d,e,f,g,h;
-	glLineWidth(1);
+	glLineWidth(2);
 	glColor3f(0.96, 0.84, 0.13);//amarillo
 	a = 0.4*s1;
 	c = 0.55*s3;
@@ -37,55 +37,79 @@ void pintarCuadrante(int s1,int s2,int s3,int s4,int s5,int s6,int s7,int s8){
 	h = 0.4*s8;
 	drawQUAD(a,0.0,c,0.0,0.0,f,0.0,h);
 	glColor3f(0.0,0.0,0.0);//negro
-	marcarContorno(a,0.0,b,0.0,0.0,c,0.0,d);
+	marcarContorno(a,0.0,c,0.0,0.0,f,0.0,h);
 	glColor3f(0.96, 0.84, 0.13);//amarillo
 	a = 1.0*s1;
+	b = 0.45*s2;
 	c = 1.0*s3;
+	d = 0.6*s4;
 	e = 0.6*s5;
-	drawQUAD(a,0.45,c,0.6,e,1.0,0.45,1.0);
+	f = 1.0*s6;
+	g = 0.45*s7;
+	h = 1.0*s8;
+	drawQUAD(a,b,c,d,e,f,g,h);
 	glColor3f(0.0,0.0,0.0);//negro
-	marcarContorno(a,0.45,1.0,0.6,0.6,1.0,0.45,1.0);
-	/* glColor3f(0.92, 0.65, 0.082);//naranja
+	marcarContorno(a,b,c,d,e,f,g,h);
+	glColor3f(0.92, 0.65, 0.082);//naranja
 	a = 0.55*s1;
-	b = 0.7*s2;
 	c = 0.7*s3;
-	d = 0.55*s4;
-	drawQUAD(a,0.0,b,0.0,0.0,c,0.0,d);
+	f = 0.7*s6;
+	h = 0.55*s8;
+	drawQUAD(a,0.0,c,0.0,0.0,f,0.0,h);
 	glColor3f(0.0,0.0,0.0);//negro
-	marcarContorno(a,0.0,b,0.0,0.0,c,0.0,d);
+	marcarContorno(a,0.0,c,0.0,0.0,f,0.0,h);
 	glColor3f(0.92, 0.65, 0.082);//naranja
 	a = 1.0*s1;
-	b = 1.0*s2;
+	b = 0.3*s2;
 	c = 1.0*s3;
-	d = 1.0*s4;
-	drawQUAD(a,0.3,b,0.45,0.45,c,0.3,d);
+	d = 0.45*s4;
+	e = 0.45*s5;
+	f = 1.0*s6;
+	g = 0.3*s7;
+	h = 1.0*s8;
+	drawQUAD(a,b,c,d,e,f,g,h);
 	glColor3f(0.0,0.0,0.0);//negro
-	marcarContorno(a,0.3,b,0.45,0.45,c,0.3,d);
+	marcarContorno(a,b,c,d,e,f,g,h);
 	glColor3f(0.02, 0.02, 0.63);//azul
 	a = 0.7*s1;
-	b = 0.77*s2;
 	c = 0.77*s3;
-	d = 0.7*s3;
-	drawQUAD(a,0.0,b,0.07,0.07,c,0.0,d);
+	d = 0.07*s4;
+	e = 0.07*s5;
+	f = 0.77*s6;
+	h = 0.7*s8;
+	drawQUAD(a,0.0,c,d,e,f,0.0,h);
 	glColor3f(0.0,0.0,0.0);//negro
-	marcarContorno(a,0.0,b,0.07,0.07,c,0.0,d);
+	marcarContorno(a,0.0,c,d,e,f,0.0,h);
 	glColor3f(0.02, 0.02, 0.63);//azul
 	a = 0.93*s1;
-	b = 1.0*s2;
+	b = 0.23*s2;
 	c = 1.0*s3;
-	d = 0.93*s4;
-	drawQUAD(a,0.23,b,0.3,0.3,c,0.23,d);
+	d = 0.3*s4;
+	e = 0.3*s5;
+	f = 1.0*s6;
+	g = 0.23*s7;
+	h = 0.93*s8;
+	drawQUAD(a,b,c,d,e,f,g,h);
 	glColor3f(0.0,0.0,0.0);//negro
-	a = 0.77;
-	b = 0.93;
-	c = 0.93;
-	d = 0.77;
-	drawQUAD(a,0.07,b,0.23,0.23,c,0.07,d); */
+	marcarContorno(a,b,c,d,e,f,g,h);
+	a = 0.77*s1;
+	b = 0.07*s2;
+	c = 0.93*s3;
+	d = 0.23*s4;
+	e = 0.23*s5;
+	f = 0.93*s6;
+	g = 0.07*s7;
+	h = 0.77*s8;
+	drawQUAD(a,b,c,d,e,f,g,h);
+	marcarContorno(a,b,c,d,e,f,g,h);
 }
 
 
 void display(void) {	
-	pintarCuadrante(-1,-1,1,1);
+	pintarCuadrante(1,1,1,1,1,1,1,1);
+	pintarCuadrante(-1,1,-1,1,-1,1,-1,1);
+	pintarCuadrante(-1,-1,-1,-1,-1,-1,-1,-1);
+	pintarCuadrante(1,-1,1,-1,1,-1,1,-1);
 }
 
 void teclado(unsigned char key, int xmouse, int ymouse) {
@@ -97,7 +121,7 @@ void main(int argc, char** argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB);
 	glutInitWindowPosition(50, 100);
-	glutInitWindowSize(400, 300);
+	glutInitWindowSize(500, 500);
 	glutCreateWindow("Mosaico freeglut");
 	init();
 	glutDisplayFunc(display);
